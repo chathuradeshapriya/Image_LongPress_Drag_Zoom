e1696c1260b83d1bef11b94f5f1b6402e029644c
Chathura Deshapriya <57343967+chathuradeshapriya@users.noreply.github.com>
2022-09-02 23:40:32 +0530

    Developed image ZoomIn and ZoomOut Option

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -21,34 +21,66 @@ class _HomeScreenState extends State<HomeScreen> {
         ]);
   }
 
-
   Offset offset = Offset.zero;
+
+  final transController = TransformationController();
+   late TapDownDetails doubleTapDetails;
+
+  handleDoubleTapDown(TapDownDetails details){
+    doubleTapDetails = details;
+  }
+
+  handleDoubleTap(){
+    if(transController.value != Matrix4.identity()){
+      transController.value = Matrix4.identity();
+    }else{
+      final position = doubleTapDetails.localPosition;
+      transController.value = Matrix4.identity()
+      ..translate(-position.dx, -position.dy)
+      ..scale(2.0);
+    }
+
+  }
+
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(' Flutter Gesture Detector'),
+        centerTitle: true,
+      ),
       body: Stack(
         children: [
           Positioned(
             left: offset.dx,
             top: offset.dy,
             child: GestureDetector(
-              child: Image.asset(
-                'assets/image.webp',
-                width: MediaQuery.of(context).size.width,
+              child: InteractiveViewer(
+                transformationController: transController,
+                child: Image.asset(
+                  'assets/image.webp',
+                  width: MediaQuery.of(context).size.width,
+                ),
               ),
-              onLongPress: () => showMessage('Long Press', context),
 
-              onPanUpdate: (details){
-                setState((){
-                  offset = Offset(offset.dx +  details.delta.dx,
-                      offset.dy + details.delta.dy);
-                });
-              },
+             /* For Long press code*/
+              // onLongPress: () => showMessage('Long Press', context),
 
-              // onDoubleTapDown: handleDoubleTapDown,
-              // onDoubleTap: handleDoubleTap,
+
+              /* For Image Drag code*/
+              // onPanUpdate: (details){
+              //   setState((){
+              //     offset = Offset(offset.dx +  details.delta.dx,
+              //         offset.dy + details.delta.dy);
+              //   });
+              // },
+
+
+              /* For Image Zoom code*/
+              onDoubleTapDown: handleDoubleTapDown,
+              onDoubleTap: handleDoubleTap,
 
             ),
           )
